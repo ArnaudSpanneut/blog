a2b077180ed6bd05d4465612a1c11b154b1849a8
Arnaud Spanneut <arnaud.spanneut@gmail.com>
2021-02-25 09:07:17 +0100

    Add canonical url

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,13 @@ module.exports = {
     },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-gatsby-cloud`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: SITE_URL,
+        stripQueryString: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
