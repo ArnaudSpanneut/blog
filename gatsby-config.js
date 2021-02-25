7c086272f3740c364aa2766dee223188eb4681aa
Arnaud Spanneut <arnaud.spanneut@gmail.com>
2021-02-25 21:52:31 +0100

    feat: update the images info

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,13 +67,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Arnaud Spanneut Blog`,
+        short_name: `ArnaudSpanneut`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
+        icon: `content/assets/arnaud-icon.png`,
       },
     },
     `gatsby-plugin-react-helmet`,
